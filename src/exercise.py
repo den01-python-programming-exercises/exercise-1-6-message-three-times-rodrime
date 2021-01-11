59eec761db8ba38fa852a8db8da19d949f0fe310
Rodrigo Espinosa <rodrisme2@gmail.com>
2021-01-11 17:32:11 +0000

    attempt

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #write your code below this line
     message = input("Write a message...")
-
+    print (message)
+    print (message)
+    print (message)
 if __name__ == '__main__':
     main()
